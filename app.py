6ac5d5cf2045c1b58bf7d1aff4f56d505eecd4cc
shubham <shubhamtwr2@gmail.com>
2024-12-20 16:59:07 +0530

    app run change

--- app.py
+++ app.py
@@ -55,4 +55,4 @@ def home():
     return render_template("index.html", theme_message=theme_message, year_results=year_results, years=sorted(df['Year'].dropna().unique()))
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True)
